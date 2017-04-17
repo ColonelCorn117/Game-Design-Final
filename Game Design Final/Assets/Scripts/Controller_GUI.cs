3b28976de33facc11af1ec396eb567ae70e694ba
ColonelCorn117 <eeverson93@gmail.com>
2017-04-17 15:53:19 -0500

    1st stage UI

---
+++ Game Design Final/Assets/Scripts/Controller_GUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller_GUI : MonoBehaviour
+{
+	//====================================================================================================
+	//Variables
+	//====================================================================================================
+
+	public static Controller_GUI ctrl_gui;
+
+	public Image bgImage;
+	public Text descrText;
+
+	//List of every image used. Values set in editor
+	public Sprite test;
+
+
+	//====================================================================================================
+	//Start Stuff and Updaters
+	//====================================================================================================
+
+	void Awake()
+	{
+		if (ctrl_gui == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			ctrl_gui = this;
+		}
+		else if (ctrl_gui != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------
+
+	void Start()
+	{
+		bgImage = GetComponentInChildren<Image>();
+		SetBackgroundImage(test);
+	}
+
+
+	//====================================================================================================
+	//Setters
+	//====================================================================================================
+
+	public void SetBackgroundImage(Sprite imageSource)
+	{
+		bgImage.sprite = imageSource;	
+	}
+
+	//----------------------------------------------------------------------------------------------------
+
+	public void SetDescriptionText(string text)
+	{
+		descrText.text = text;
+	}
+}
---
+++ Game Design Final/Assets/Scripts/Controller_Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Game : MonoBehaviour
+{
+	//====================================================================================================
+	//Variables
+	//====================================================================================================
+
+	public static Controller_Game ctrl_game;
+	public static Controller_GUI ctrl_gui;
+
+
+	//====================================================================================================
+	//Start Stuff and Updaters
+	//====================================================================================================
+
+	void Awake()
+	{
+		if (ctrl_game == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			ctrl_game = this;
+			ctrl_gui = Controller_GUI.ctrl_gui;
+		}
+		else if (ctrl_game != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+}
