5ec6bcf22270fe4f928c26a5e2302b8de7e7278e
ibstudent220 <ibstudent220@gmail.com>
2017-04-18 12:30:20 -0500

    Fixed Deserialization Issue

---
+++ Game Design Final/Assets/DescriptionTester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+[XmlRoot("SceneDescription")]
+public class SceneDescription
+{
+	public String sceneName;
+	public String description;
+
+	public OptionList optionList;
+
+
+	public class Option
+	{
+		public String optionDescription;
+		public Action action;
+	}
+
+	public class Action {
+		public String nextScene;
+	}
+		
+	public class OptionList 
+	{
+		[XmlElement("option")]
+		public List<Option> Options = new List<Option>();
+
+
+
+
+	}
+}
+
+
+
+
