4903a16271208f79151c8063f8279f651aa6a050
ColonelCorn117 <eeverson93@gmail.com>
2017-04-17 20:39:50 -0500

    Button Functions
    Added functionality to inventory. Options 1 and 2 text temporarily
    changed to reflect what they currently do. Items button now
    expands/collapses inventory

--- Game Design Final/Assets/Scripts/Controller_Game.cs
+++ Game Design Final/Assets/Scripts/Controller_Game.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Controller_Game : MonoBehaviour
 {
+	//This script needs to be attached to the canvas to work properly.
+
 	//====================================================================================================
 	//Variables
 	//====================================================================================================
 
 	public static Controller_Game ctrl_game;
 	public static Controller_GUI ctrl_gui;
+
+	public List<string> items = new List<string>();
 
 
 	//====================================================================================================
@@ -23,6 +27,7 @@ public class Controller_Game : MonoBehaviour
 			DontDestroyOnLoad(gameObject);
 			ctrl_game = this;
 			ctrl_gui = Controller_GUI.ctrl_gui;
+			items.Add("Mop");
 		}
 		else if (ctrl_game != this)
 		{
@@ -30,5 +35,29 @@ public class Controller_Game : MonoBehaviour
 		}
 	}
 
+
+	//====================================================================================================
+	//Other Functions
+	//====================================================================================================
+
+	public void AddItemToInv()//string item)
+	{
+		items.Add("item" + items.Count);
+		ctrl_gui.ScaleItemsList();
+		Debug.Log("Item Added: " + items[items.Count - 1]);
+	}
+
+	//----------------------------------------------------------------------------------------------------
+
+	public void RemoveItemFromInv()//string item)
+	{
+		items.Remove("item" + (items.Count - 1));
+		ctrl_gui.DestroyBtnsOnItemsList();
+		if (items.Count > 0)
+		{
+			Debug.Log("Item Removed: " + items[items.Count - 1]);
+		}
+
+	}
 
 }
